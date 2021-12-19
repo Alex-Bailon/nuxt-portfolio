e039d7bbabf94a321b76c63a4bccdbfbdd16ece3
Alex-Bailon <abailon949@gmail.com>
2021-12-19 17:37:55 -0600

    created index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,101 @@
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({$axios}) {
+    const timeline = await $axios.get('https://api.cosmicjs.com/v2/buckets/nuxt-portfolio-production/objects?pretty=true&query=%7B%22type%22%3A%22time-lines%22%7D&read_key=LPx3LELVgjXcFbGKD3xjBQGNVd87FsFbK9bbCeO9MJ8lFGNcLN&limit=20&props=slug,title,content,metadata')
+    const aboutme = await $axios.get('https://api.cosmicjs.com/v2/buckets/nuxt-portfolio-production/objects/6056606482408b0007b7f1d2?pretty=true&read_key=LPx3LELVgjXcFbGKD3xjBQGNVd87FsFbK9bbCeO9MJ8lFGNcLN&props=slug,title,content,metadata')
+    return {
+      socials: [
+        {
+          icon: 'mdi-github',
+          color: 'purple darken-1',
+          link: 'https://github.com/Alex-Bailon'
+        },
+        {
+          icon: 'mdi-linkedin',
+          color: 'cyan darken-1',
+          link: 'https://www.linkedin.com/in/alex-bailon'
+        },
+      ],
+      timelineItems: timeline.data.objects,
+      tabs: [
+        {
+          title: 'About Me',
+          component: 'Aboutme'
+        },
+        {
+          title: 'Portfolio',
+          component: 'Portfolio'
+        },
+        {
+          title: 'Contact',
+          component: 'Contact'
+        },                
+      ],
+      tabSelected: null,
+      aboutme: aboutme.data.object
+    }
+  },
+}
+</script>
+
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+    <v-col cols="12" md="4">
+      <v-card class="mx-auto" max-width="344">
+        <v-card-title class="pt-2">
+          Alex Bailon
         </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+        <v-card-subtitle class="pb-0">
+          Full Stack Developer <br/>
           <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
+            v-for="(social, i) in socials"
+            :key="i"
+            :color="social.color"
+            class="white--text"
+            fab
+            icon
+            small
+            :href="social.link"
+            target="_blank"
           >
-            Continue
+            <v-icon large>{{ social.icon }}</v-icon>
           </v-btn>
-        </v-card-actions>
+        </v-card-subtitle>
+        <v-card-text>
+          <v-img src="https://raw.githubusercontent.com/Alex-Bailon/Alex-Bailon.github.io/master/assets/images/AlexGCWest.jpg" />
+          <v-timeline dense>
+            <v-timeline-item v-for="( item, i ) in timelineItems" :key="i" :color="item.metadata.color" :icon="item.metadata.icon" fill-dot>
+              <p><strong>{{ item.metadata.title }}:</strong> <br/> {{ item.metadata.text }}</p>
+            </v-timeline-item>
+          </v-timeline>
+          <v-btn block depressed outlined href="https://alex-bailon.github.io/assets/images/A_Bailon_Resume.pdf" target="_blank">
+            <v-icon left>mdi-file-document</v-icon> Resume
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="8">
+      <v-card>
+        <v-tabs v-model="tabSelected" show-arrows right grow>
+          <v-tab v-for="tab in tabs" :key="tab.title">
+            {{ tab.title }}
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tabSelected">
+          <v-tab-item
+            v-for="tab in tabs"
+            :key="tab.component"
+          >
+            <v-card flat>
+              <v-card-text>
+                <component :is="tab.component" :aboutme="aboutme"></component>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
-<script>
-export default {
-  name: 'IndexPage'
-}
-</script>
