c63b0face9dff6493849fa1d06923700e097ee6e
Alex-Bailon <abailon949@gmail.com>
2021-12-19 17:38:11 -0600

    created about me component

---
+++ components/aboutme.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'About',
+  props: {
+    aboutme: {
+      default: []
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ aboutme.metadata.p1 }}</p>
+    <p>{{ aboutme.metadata.p2 }}</p>
+    <p>{{ aboutme.metadata.p3 }}</p>
+    <p>{{ aboutme.metadata.p4 }}</p>      
+    <blockquote class="blockquote py-2"><p><em>{{ aboutme.metadata.quote }}<br/>- {{ aboutme.title }}</em></p></blockquote>
+  </div>
+</template>
