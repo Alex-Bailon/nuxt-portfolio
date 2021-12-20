7311880980c94caea4cf06794f2c820e22dd12a2
Alex-Bailon <abailon949@gmail.com>
2021-12-19 18:24:24 -0600

    Create contact.vue

---
+++ components/contact.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'Contact',
+  data(){
+    return {}
+  },
+  computed: {
+    required() {
+      return v => !!v || 'Field is required'
+    },
+    emailRules() {
+      return [
+        v => v.length <= 320 || 'Email is too long',
+        v => /\S+@\S+\.\S+/.test(v) || 'E-mail must be valid',
+      ]     
+    }
+  },
+}
+</script>
+
+<template>
+  <h2>
+    For the quickest response please reach out to me on <a href="https://www.linkedin.com/in/alex-bailon">LinkedIn</a>
+  </h2>
+</template>
