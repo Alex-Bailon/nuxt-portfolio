c19e2fb958fd8933201053e86c2df677cde01af3
Alex-Bailon <abailon949@gmail.com>
2021-12-20 08:39:10 -0600

    updated title and alt text

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'Alex Bailon',
-    title: 'Alex Bailon',
+    titleTemplate: '%s | Portfolio',
+    title: 'Alex Bailon | Full Stack Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
