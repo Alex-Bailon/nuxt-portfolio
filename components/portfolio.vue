b32b43d47f2b886a4bd09da18c016f749025daf8
Alex-Bailon <abailon949@gmail.com>
2021-12-19 18:24:22 -0600

    Create portfolio.vue

---
+++ components/portfolio.vue
@@ -0,0 +1,29 @@
+<script>
+  export default {
+    name: 'Portfolio',
+    props: {
+      projects: {
+        default: []
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <v-row class="px-4 d-flex justify-space-around">
+      <v-col  v-for="project in projects" :key="project.title" cols="12" :sm="project.metadata.col">
+        <v-card min-height="425">
+          <v-img :src="project.metadata.img" :height="project.metadata.height"/>
+          <v-card-title>{{ project.title }}</v-card-title>
+          <v-card-text>{{ project.metadata.description }} <br/> <a target="_blank" :href="project.metadata.live">Link to live site</a> <br/> <a target="_blank" :href="project.metadata.github">Link to Github Repository</a> </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="text-body-2 text-center">
+        <p>To view more of my project please vist my <a href="http://github.com/Alex-Bailon" target="_blank">Github</a> or feel free to contact me <a href="mailto:abailon949@gmail.com">here</a>.</p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
